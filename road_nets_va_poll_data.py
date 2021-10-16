fe0f947e4a15c54295cea9baef9b7506e238d96a
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2021-10-16 17:44:17 -0400

    creates pollution data for VA using EPA AQS data

--- road_nets_va_poll_data.py
+++ road_nets_va_poll_data.py
@@ -19,49 +19,21 @@ no2 = pd.read_csv(filepath + 'data/pollution_data/no2_data.csv')
 
 # Creating a list of all VA FIPS codes and municipalities
 
-fips_list = [51003,51005,51007,51009,51011,51013,51015,51017,51019,51021,51023,
-             51025,51027,51029,51031,51033,51035,51036,51037,51041,51043,51045,
-             51047,51049,51051,51053,51057,51059,51061,51063,51065,51067,51069,
-             51071,51073,51075,51077,51079,51081,51083,51085,51087,51089,51091,
-             51093,51095,51097,51099,51101,51103,51105,51107,51109,51111,51113,
-             51115,51117,51119,51121,51125,51127,51133,51135,51137,51139,51141,
-             51143,51145,51147,51149,51153,51155,51157,51159,51161,51163,51165,
-             51167,51169,51171,51173,51175,51177,51179,51181,51183,51185,51187,
-             51191,51193,51195,51197,51199,51510,51520,51530,51540,51550,51570,
-             51580,51590,51595,51600,51610,51620,51630,51640,51650,51660,51670,
-             51678,51680,51683,51685,51690,51700,51710,51720,51730,51735,51740,
-             51750,51760,51770,51775,51790,51800,51810,51820,51830,51840]
+fips_list = [51810, 51013, 51059, 51107, 51003, 51069, 51775, 51165, 51710, 51087,
+             51680, 51041, 51036, 51520, 51650, 51161, 51760, 51670, 51840, 51035,
+             51179, 51153, 51071, 51113, 51147, 51041, 51163, 51033, 51061, 51800,
+             51085, 51197, 51510, 51630, 51700, 51770, 51047, 51101, 51139, 51187]
 
-munic_list = ['AlbemarleCounty','AlleghanyCounty','AmeliaCounty','AmherstCounty',
-              'AppomattoxCounty','ArlingtonCounty','AugustaCounty','BathCounty',
-              'BedfordCounty','BlandCounty','BotetourtCounty','BrunswickCounty',
-              'BuchananCounty','BuckinghamCounty','CampbellCounty','CarolineCounty',
-              'CarrollCounty','CharlesCityCounty','CharlotteCounty','ChesterfieldCounty',
-              'ClarkeCounty','CraigCounty','CulpeperCounty','CumberlandCounty',
-              'DickensonCounty','DinwiddieCounty','EssexCounty','FairfaxCounty',
-              'FauquierCounty','FloydCounty','FluvannaCounty','FranklinCounty',
-              'FrederickCounty','GilesCounty','GloucesterCounty','GoochlandCounty',
-              'GraysonCounty','GreeneCounty','GreensvilleCounty','HalifaxCounty',
-              'HanoverCounty','HenricoCounty','HenryCounty','HighlandCounty',
-              'IsleofWightCounty','JamesCityCounty','KingandQueenCounty','KingGeorgeCounty',
-              'KingWilliamCounty','LancasterCounty','LeeCounty','LoudounCounty',
-              'LouisaCounty','LunenburgCounty','MadisonCounty','MathewsCounty',
-              'MecklenburgCounty','MiddlesexCounty','MontgomeryCounty','NelsonCounty',
-              'NewKentCounty','NorthumberlandCounty','NottowayCounty',
-              'OrangeCounty','PageCounty','PatrickCounty','PittsylvaniaCounty',
-              'PowhatanCounty','PrinceEdwardCounty','PrinceGeorgeCounty','PrinceWilliamCounty',
-              'PulaskiCounty','RappahannockCounty','RichmondCounty','RoanokeCounty',
-              'RockbridgeCounty','RockinghamCounty','RussellCounty','ScottCounty',
-              'ShenandoahCounty','SmythCounty','SouthamptonCounty','SpotsylvaniaCounty',
-              'StaffordCounty','SurryCounty','SussexCounty','TazewellCounty','WarrenCounty',
-              'WashingtonCounty','WestmorelandCounty','WiseCounty','WytheCounty',
-              'YorkCounty','Alexandria','Bristol','BuenaVista','Charlottesville',
-              'Chesapeake','ColonialHeights','Covington','Danville','Emporia',
-              'Fairfax','FallsChurch','Franklin','Fredericksburg','Galax','Hampton',
-              'Harrisonburg','Hopewell','Lexington','Lynchburg','Manassas','ManassasPark',
-              'Martinsville','NewportNews','Norfolk','Norton','Petersburg','Poquoson',
-              'Portsmouth','Radford','Richmond','Roanoke','Salem','Staunton','Suffolk',
-              'VirginiaBeach','Waynesboro','Williamsburg','Winchester']
+
+munic_list = ['VirginiaBeach', 'ArlingtonCounty', 'FairfaxCounty', 'LoudounCounty',
+              'AlbemarleCounty', 'FrederickCounty', 'Salem', 'RockinghamCounty',
+              'Norfolk', 'HenricoCounty', 'Lynchburg', 'ChesterfieldCounty',
+              'CharlesCityCounty', 'Bristol', 'Hampton', 'RoanokeCounty', 'Richmond',
+              'Hopewell', 'Winchester', 'CarrollCounty', 'StaffordCounty', 'PrinceWilliamCounty',
+              'GilesCounty', 'MadisonCounty', 'PrinceEdwardCounty', 'RockbridgeCounty',
+              'CarolineCounty', 'FauquierCounty', 'Suffolk', 'HanoverCounty', 'WytheCounty',
+              'Alexandria', 'Fredericksburg', 'NewportNews', 'Roanoke', 'CulpeperCounty',
+              'KingWilliamCounty', 'PageCounty', 'WarrenCounty']
 
 # Defining the sample
 
@@ -103,7 +75,7 @@ for d in dates:
         for c in counties + cities:
             
             locations.append(c)
-            fips.append(fips_list[agg_list.index(c)])
+            fips.append(fips_list[munic_list.index(c)])
             polls.append(pollutants[p])
             df_dates.append(d)
             
@@ -129,7 +101,7 @@ for d in dates:
                 
             try:
                 
-                tmp = tmp[tmp.FIPS == fips_list[agg_list.index(c)]].reset_index(drop = True)
+                tmp = tmp[tmp.FIPS == fips_list[munic_list.index(c)]].reset_index(drop = True)
                 vp = tmp.Value[0]
                 vals.append(vp)
                 
