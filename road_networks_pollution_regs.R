d4474989aaccdd451e3379441bf5c4546d97d8d0
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2021-10-18 23:09:03 -0400

    main econometrics script

---
+++ road_networks_pollution_regs.R
@@ -0,0 +1,13 @@
+# Main econometrics script for a paper on the impact of road network topology on pollution
+
+# Running the individual scripts for each set of analyses -- update username & ensure that the directory is correct
+
+username = ''
+diRectoRy <- paste('C:/Users/', username, '/Documents/', sep = '')
+
+# Running the R scripts
+
+source(paste(diRectoRy, 'road_networks_pollution_regs_ht.R', sep = ''))
+source(paste(diRectoRy, 'road_networks_pollution_regs_diff.R', sep = ''))
+source(paste(diRectoRy, 'road_networks_pollution_regs_iv.R', sep = ''))
+
