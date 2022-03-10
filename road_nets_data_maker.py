4ae703b04d6b98cd709aed6ecb46eb74b36b4122
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2022-03-09 22:47:50 -0500

    creates the final data set for the project

--- road_nets_data_maker.py
+++ road_nets_data_maker.py
@@ -47,7 +47,7 @@ for d in dates:
     tmp = poll[poll.Date == d]
     co_tmp = tmp[tmp.Pollutant == 'CO']
     no2_tmp = tmp[tmp.Pollutant == 'NO2']
-    o3_tmp = tmp[tmp.Pollutant == 'O3']
+    o3_tmp = tmp[tmp.Pollutant == 'Ozone']
     pm_tmp = tmp[tmp.Pollutant == 'PM2.5']
     pm10_tmp = tmp[tmp.Pollutant == 'PM10']
     
