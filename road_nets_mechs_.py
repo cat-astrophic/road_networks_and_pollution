29b460601dbc05d1bf9f6b5ad705961b30fbd633
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2022-06-16 22:29:53 -0400

    creates topological indices for mechanisms municipalities

--- road_nets_mechs_.py
+++ road_nets_mechs_.py
@@ -55,71 +55,82 @@ for c in locations:
     
     print(c)
     
-    # Generate the network from OSM with OSMnx
-    
-    G = ox.graph_from_place(c + ', USA', network_type = 'drive')
-    
-    # Impute speed on all edges missing data
-    
-    G = ox.add_edge_speeds(G)
-    
-    # Calculate travel time (seconds) for all edges
-    
-    G = ox.add_edge_travel_times(G)
-    
-    # Ensure that it is strongly connected
-    
-    Gc = ox.utils_graph.get_largest_component(G, strongly = True)
-    
-    # Additional street network / municipality data
-    
-    gdf = ox.geocode_to_gdf(c + ', USA')
-    area = ox.project_gdf(gdf).unary_union.area
-    
-    # Built-in OSMnx stats
-    
-    stats = ox.basic_stats(Gc, area = area)
-    ox_stats = pd.Series(stats)
-    
-    # Create a network figure
-    
-    fig, ax = ox.plot_graph(Gc)
-    fig.savefig(figs_direc + c.replace(' ', '_') + '__network.eps')
-    fig.savefig(figs_direc + c.replace(' ', '_') + '__network.png')
-    
-    # Create an edge centrality figure
-    
-    edge_centrality = nx.closeness_centrality(nx.line_graph(Gc))
-    ev = [edge_centrality[edge + (0,)] for edge in Gc.edges()]
-    norm = colors.Normalize(vmin = min(ev)*0.8, vmax = max(ev))
-    cmap = cm.ScalarMappable(norm = norm, cmap = cm.inferno)
-    ec = [cmap.to_rgba(cl) for cl in ev]
-    fig, ax = ox.plot_graph(Gc, bgcolor = 'k', node_size = 0, node_color = 'w',
-                            node_edgecolor = 'gray', node_zorder = 2, edge_color = ec,
-                            edge_linewidth = 1.5, edge_alpha = 1)
-    fig.savefig(figs_direc + c.replace(' ', '_') + '__edge_centrality.eps')
-    fig.savefig(figs_direc + c.replace(' ', '_') + '__edge_centrality.png')
-    
-    # Setup for nx.percolation_centrality
-    
-    nx.set_node_attributes(Gc, 0.1, 'percolation')
-    percolation_cent_dict = nx.percolation_centrality(G = Gc, attribute = 'percolation')    
-    
-    # Generating network statistics
-    
-    ebc = nx.edge_betweenness_centrality(Gc)
-    lc = nx.load_centrality(Gc)
-    
-    col_names.append(c)
-    edge_betweenness_centrality_mean.append(sum([ebc[k] for k in ebc.keys()]) / Gc.size())
-    load_centrality_mean.append(sum([lc[k] for k in Gc]) / len(Gc))
-    
-    # Grab some stats from ox_stats Series
-    
-    circuity.append(ox_stats['circuity_avg'])
-    pct_4way_intersections.append(ox_stats['streets_per_node_proportions'][4])
-    pct_3way_intersections.append(ox_stats['streets_per_node_proportions'][3])
+    try:
+        
+        # Generate the network from OSM with OSMnx
 
+        G = ox.graph_from_place(c + ', USA', network_type = 'drive')
+
+        # Impute speed on all edges missing data
+
+        G = ox.add_edge_speeds(G)
+
+        # Calculate travel time (seconds) for all edges
+
+        G = ox.add_edge_travel_times(G)
+
+        # Ensure that it is strongly connected
+
+        Gc = ox.utils_graph.get_largest_component(G, strongly = True)
+
+        # Additional street network / municipality data
+
+        gdf = ox.geocode_to_gdf(c + ', USA')
+        area = ox.project_gdf(gdf).unary_union.area
+
+        # Built-in OSMnx stats
+
+        stats = ox.basic_stats(Gc, area = area)
+        ox_stats = pd.Series(stats)
+
+        # Create a network figure
+
+        fig, ax = ox.plot_graph(Gc)
+        fig.savefig(figs_direc + c.replace(' ', '_') + '__network.eps')
+        fig.savefig(figs_direc + c.replace(' ', '_') + '__network.png')
+
+        # Create an edge centrality figure
+
+        edge_centrality = nx.closeness_centrality(nx.line_graph(Gc))
+        ev = [edge_centrality[edge + (0,)] for edge in Gc.edges()]
+        norm = colors.Normalize(vmin = min(ev)*0.8, vmax = max(ev))
+        cmap = cm.ScalarMappable(norm = norm, cmap = cm.inferno)
+        ec = [cmap.to_rgba(cl) for cl in ev]
+        fig, ax = ox.plot_graph(Gc, bgcolor = 'k', node_size = 0, node_color = 'w',
+                                node_edgecolor = 'gray', node_zorder = 2, edge_color = ec,
+                                edge_linewidth = 1.5, edge_alpha = 1)
+        fig.savefig(figs_direc + c.replace(' ', '_') + '__edge_centrality.eps')
+        fig.savefig(figs_direc + c.replace(' ', '_') + '__edge_centrality.png')
+
+        # Setup for nx.percolation_centrality
+
+        nx.set_node_attributes(Gc, 0.1, 'percolation')
+        percolation_cent_dict = nx.percolation_centrality(G = Gc, attribute = 'percolation')    
+
+        # Generating network statistics
+
+        ebc = nx.edge_betweenness_centrality(Gc)
+        lc = nx.load_centrality(Gc)
+
+        col_names.append(c)
+        edge_betweenness_centrality_mean.append(sum([ebc[k] for k in ebc.keys()]) / Gc.size())
+        load_centrality_mean.append(sum([lc[k] for k in Gc]) / len(Gc))
+
+        # Grab some stats from ox_stats Series
+
+        circuity.append(ox_stats['circuity_avg'])
+        pct_4way_intersections.append(ox_stats['streets_per_node_proportions'][4])
+        pct_3way_intersections.append(ox_stats['streets_per_node_proportions'][3])
+        
+    except:
+        
+        col_names.append(c)
+        edge_betweenness_centrality_mean.append(' ')
+        load_centrality_mean.append(' ')
+        circuity.append(' ')
+        pct_4way_intersections.append(' ')
+        pct_3way_intersections.append(' ')
+        
 # Appending network data to df
 
 edge_betweenness_centrality_mean = pd.Series(edge_betweenness_centrality_mean, name = 'Edge Betweenness Centrality')
@@ -132,8 +143,9 @@ df = pd.concat([df, edge_betweenness_centrality_mean, load_centrality_mean, circ
                 pct_4way_intersections, pct_3way_intersections], axis = 1)
 
 df.index = col_names
-df.columns = ['Locations', 'edge_betweenness_centrality_mean', 'load_centrality_mean',
+df.columns = ['edge_betweenness_centrality_mean', 'load_centrality_mean',
               'circuity', 'pct_4way_intersections', 'pct_3way_intersections']
+df.index.name = 'Locations'
 
 # Write network statistics to file
 
