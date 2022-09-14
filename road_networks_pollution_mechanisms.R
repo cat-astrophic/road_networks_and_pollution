df1daa7709ae202a9901c62de44293240a028dcd
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2022-09-13 23:36:37 -0400

    mechanisms models

--- road_networks_pollution_mechanisms.R
+++ road_networks_pollution_mechanisms.R
@@ -28,9 +28,9 @@ mc <- merge(cities, mechdata, by.x = 'Locations', by.y = 'Location')
 
 # Running regressions
 
-c1 <- lm(log(Congestion2) ~ log(Edge.Betweenness.Centrality) + log(Population) + Capital + log(Density.sqmi) + log(Trips), data = mc)
-c2 <- lm(log(Congestion2) ~ log(Load.Centrality) + log(Population) + Capital + log(Density.sqmi) + log(Trips), data = mc)
-c3 <- lm(log(Congestion2) ~ log(Circuity) + log(Population) + Capital + log(Density.sqmi) + log(Trips), data = mc)
+c1 <- ivreg(Congestion2 ~ Edge.Betweenness.Centrality + Capital + log(Trips) + log(Population) | . - Edge.Betweenness.Centrality + Density.sqmi + log(Time), data = mc)
+c2 <- ivreg(Congestion2 ~ Load.Centrality + Capital + log(Trips) + log(Population) | . - Load.Centrality + Density.sqmi + log(Time), data = mc)
+c3 <- ivreg(Congestion2 ~ Circuity + Capital + log(Trips) + log(Population) | . - Circuity + Density.sqmi + log(Time), data = mc)
 
 c1x <- coeftest(c1, vcov = vcovCL, cluster = ~Capital)
 c2x <- coeftest(c2, vcov = vcovCL, cluster = ~Capital)
@@ -38,36 +38,13 @@ c3x <- coeftest(c3, vcov = vcovCL, cluster = ~Capital)
 
 stargazer(c1x,c2x,c3x, type = 'text')
 
-t1 <- lm(log(Time) ~ log(Edge.Betweenness.Centrality) + log(Population) + Capital + log(Density.sqmi) + log(Trips), data = mc)
-t2 <- lm(log(Time) ~ log(Load.Centrality) + log(Population) + Capital + log(Density.sqmi) + log(Trips), data = mc)
-t3 <- lm(log(Time) ~ log(Circuity) + log(Population) + Capital + log(Density.sqmi) + log(Trips), data = mc)
+t1 <- ivreg(log(Time) ~ Edge.Betweenness.Centrality + Capital + log(Trips) + log(Population) | . - Edge.Betweenness.Centrality + Density.sqmi + log(Time), data = mc)
+t2 <- ivreg(log(Time) ~ Load.Centrality + Capital + log(Trips) + log(Population) | . - Load.Centrality + Density.sqmi + log(Time), data = mc)
+t3 <- ivreg(log(Time) ~ Circuity + Capital + log(Trips) + log(Population) | . - Circuity + Density.sqmi + log(Time), data = mc)
 
 t1x <- coeftest(t1, vcov = vcovCL, cluster = ~Capital)
 t2x <- coeftest(t2, vcov = vcovCL, cluster = ~Capital)
 t3x <- coeftest(t3, vcov = vcovCL, cluster = ~Capital)
 
 stargazer(t1x,t2x,t3x, type = 'text')
-
-# Running IV regressions
-
-C.E <- log(Congestion2) ~ log(Edge.Betweenness.Centrality) + log(Population) + Capital + log(Trips) + log(Density.sqmi)
-T.E <- log(Time) ~ log(Edge.Betweenness.Centrality) + log(Population) + Capital + log(Trips) + log(Density.sqmi)
-sys <- list(C.E,T.E)
-instr <- ~ log(Population) + Capital + log(Trips) + log(Density.sqmi) + log(Edge.Betweenness.Centrality)
-contime.system.ebcm <- systemfit(sys, inst = instr, method = '2SLS', data = mc)
-summary(contime.system.ebcm)
-
-C.E <- log(Congestion2) ~ log(Load.Centrality) + log(Population) + Capital + log(Trips) + log(Density.sqmi)
-T.E <- log(Time) ~ log(Load.Centrality) + log(Population) + Capital + log(Trips) + log(Density.sqmi)
-sys <- list(C.E,T.E)
-instr <- ~ log(Population) + Capital + log(Trips) + log(Density.sqmi) + log(Load.Centrality)
-contime.system.lcm <- systemfit(sys, inst = instr, method = '2SLS', data = mc)
-summary(contime.system.lcm)
-
-C.E <- log(Congestion2) ~ log(Circuity) + log(Population) + Capital + log(Trips) + log(Density.sqmi)
-T.E <- log(Time) ~ log(Circuity) + log(Population) + Capital + log(Trips) + log(Density.sqmi)
-sys <- list(C.E,T.E)
-instr <- ~ log(Population) + Capital + log(Trips) + log(Density.sqmi) + log(Circuity)
-contime.system.circuity <- systemfit(sys, inst = instr, method = '2SLS', data = mc)
-summary(contime.system.circuity)
 
