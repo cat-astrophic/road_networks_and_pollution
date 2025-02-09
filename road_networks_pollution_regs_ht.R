567c083e04a3fea359e7a36a97f0a7c7d28cc987
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2025-02-09 12:34:55 -0500

    main (hausman-taylor) models

--- road_networks_pollution_regs_ht.R
+++ road_networks_pollution_regs_ht.R
@@ -20,6 +20,7 @@ filepath <- paste('C:/Users/', username, '/Documents/Data/road_networks/data/', 
 
 regdata <- read.csv(paste(filepath, 'complete_data.csv', sep = ''))
 covdata <- read.csv(paste(filepath, 'coVAriates.csv', sep = ''))
+rugged <- read.csv(paste(filepath, 'RuggednessScale2010tracts.csv', sep = ''))
 
 # Creating a list of network descriptors
 
@@ -86,6 +87,60 @@ regdata$Vehicles <- cars
 regdata$UIC <- uic
 regdata$Income <- mhhi
 
+# Adding ruggedness data
+
+rugged <- rugged[which(rugged$State == 'VA'),]
+
+r.fips <- unique(rugged$CountyFIPS)
+r.area <- c()
+r.road <- c()
+r.area.sd <- c()
+r.road.sd <- c()
+
+for (f in r.fips) {
+  
+  tmp <- rugged[which(rugged$CountyFIPS == f),]
+  r.area <- c(r.area, mean(tmp$AreaTRI_Mean))
+  r.road <- c(r.road, mean(tmp$RoadTRI_Mean))
+  r.area.sd <- c(r.area.sd, mean(tmp$AreaTRI_StdDev))
+  r.road.sd <- c(r.road.sd, mean(tmp$RoadTRI_StdDev))
+  
+}
+
+rugged.df <- as.data.frame(cbind(r.fips, r.area, r.road, r.area.sd, r.road.sd))
+
+names <- c('Albemarle County', 'Arlington County', 'Caroline County', 'Carroll County', 'Charles City County', 'Chesterfield County', 'Culpeper County', 'Fairfax County', 'Fauquier County', 'Frederick County', 'Giles County', 'Hanover County', 'Henrico County', 'King William County', 'Loudoun County', 'Madison County', 'Page County', 'Prince Edward County', 'Prince William County', 'Roanoke County', 'Rockbridge County', 'Rockingham County', 'Stafford County', 'Warren County', 'Wythe County', 'Alexandria', 'Fredericksburg', 'Hampton', 'Hopewell', 'Lynchburg', 'Newport News', 'Norfolk', 'Richmond', 'Roanoke', 'Salem', 'Suffolk', 'Virginia Beach', 'Winchester')
+fips <- c(51003, 51013, 51033, 51035, 51036, 51041, 51047, 51059, 51061, 51069, 51071, 51085, 51087, 51101, 51107, 51113, 51139, 51147, 51153, 51161, 51163, 51165, 51179, 51187, 51197, 51510, 51630, 51650, 51670, 51680, 51700, 51710, 51760, 51770, 51775, 51800, 51810, 51840)
+reg.fips <- c()
+
+for (i in 1:nrow(regdata)) {
+  
+  reg.fips <- c(reg.fips, fips[which(names == regdata$Locations[i])])
+  
+}
+
+regdata$FIPS <- reg.fips
+
+reg.area <- c()
+reg.road <- c()
+reg.area.sd <- c()
+reg.road.sd <- c()
+
+for (i in 1:nrow(regdata)) {
+  
+  tmp <- rugged.df[which(rugged.df$r.fips == regdata$FIPS[i]),]
+  reg.area <- c(reg.area, tmp$r.area[1])
+  reg.road <- c(reg.road, tmp$r.road[1])
+  reg.area.sd <- c(reg.area.sd, tmp$r.area.sd[1])
+  reg.road.sd <- c(reg.road.sd, tmp$r.road.sd[1])
+  
+}
+
+regdata$Rugged_Area <- reg.area
+regdata$Rugged_Road <- reg.road
+regdata$Rugged_Area_SD <- reg.area.sd
+regdata$Rugged_Road_SD <- reg.road.sd
+
 # Sorting the data
 
 regdata <- regdata[order(regdata$Locations, regdata$Time),]
@@ -137,34 +192,34 @@ regdata$PM2.5_Diff <- pm_diffs
 
 # Running Hausman-Taylor IV regressions for stocks
 
-ebcm_mod_pm <- plm(PM2.5 ~ edge_betweenness_centrality_mean + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - edge_betweenness_centrality_mean + Density + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
-lcm_mod_pm <- plm(PM2.5 ~ load_centrality_mean + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - load_centrality_mean + Density + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
-spm_mod_pm <- plm(PM2.5 ~ shortest_path_mean + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - shortest_path_mean + Density + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
-circuity_mod_pm <- plm(PM2.5 ~ circuity + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - circuity + Density + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
-pct3_mod_pm <- plm(PM2.5 ~ pct_3way_intersections + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - pct_3way_intersections + Density + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
+ebcm_mod_pm <- plm(PM2.5 ~ edge_betweenness_centrality_mean + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - edge_betweenness_centrality_mean + Rugged_Area + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
+lcm_mod_pm <- plm(PM2.5 ~ load_centrality_mean + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - load_centrality_mean + Rugged_Area + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
+spm_mod_pm <- plm(PM2.5 ~ shortest_path_mean + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - shortest_path_mean + Rugged_Area + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
+circuity_mod_pm <- plm(PM2.5 ~ circuity + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - circuity + Rugged_Area + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
+pct3_mod_pm <- plm(PM2.5 ~ pct_3way_intersections + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - pct_3way_intersections + Rugged_Area + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
 
-stargazer(ebcm_mod_pm, lcm_mod_pm, spm_mod_pm, circuity_mod_pm, pct3_mod_pm, type = 'text')
+stargazer(ebcm_mod_pm, lcm_mod_pm, spm_mod_pm, circuity_mod_pm, pct3_mod_pm, type = 'text', omit = c('Week', 'Locations'))
 
 # Running Hausman-Taylor IV regressions for flows
 
-ebcm_mod_pmd <- plm(PM2.5_Diff ~ edge_betweenness_centrality_mean + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - edge_betweenness_centrality_mean + Density + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
-lcm_mod_pmd <- plm(PM2.5_Diff ~ load_centrality_mean + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - load_centrality_mean + Density + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
-spm_mod_pmd <- plm(PM2.5_Diff ~ shortest_path_mean + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - shortest_path_mean + Density + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
-circuity_mod_pmd <- plm(PM2.5_Diff ~ circuity + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - circuity + Density + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
-pct3_mod_pmd <- plm(PM2.5_Diff ~ pct_3way_intersections + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - pct_3way_intersections + Density + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
+ebcm_mod_pmd <- plm(PM2.5_Diff ~ edge_betweenness_centrality_mean + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - edge_betweenness_centrality_mean + Rugged_Area + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
+lcm_mod_pmd <- plm(PM2.5_Diff ~ load_centrality_mean + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - load_centrality_mean + Rugged_Area + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
+spm_mod_pmd <- plm(PM2.5_Diff ~ shortest_path_mean + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - shortest_path_mean + Rugged_Area + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
+circuity_mod_pmd <- plm(PM2.5_Diff ~ circuity + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - circuity + Rugged_Area + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
+pct3_mod_pmd <- plm(PM2.5_Diff ~ pct_3way_intersections + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday) | . - pct_3way_intersections + Rugged_Area + TEMP + PRCP + WDSP + factor(Locations) + factor(Week) + factor(Weekday), data = regdata, random.method = 'ht', model = 'random')
 
-stargazer(ebcm_mod_pmd, lcm_mod_pmd, spm_mod_pmd, circuity_mod_pmd, pct3_mod_pmd, type = 'text')
+stargazer(ebcm_mod_pmd, lcm_mod_pmd, spm_mod_pmd, circuity_mod_pmd, pct3_mod_pmd, type = 'text', omit = c('Week', 'Locations'))
 
 # Cute sum stats table example
 
 keepers <- c('PM2.5', 'edge_betweenness_centrality_mean', 'load_centrality_mean', 'circuity',
-             'pct_3way_intersections', 'Density', 'TEMP', 'PRCP', 'WDSP')
+   'pct_3way_intersections', 'Rugged_Area', 'TEMP', 'PRCP', 'WDSP')
 
 sumdata <- regdata[,names(regdata) %in% keepers]
 
 new_names <- c('Mean Edge Betweenness Centrality', 'Mean Load Centrality',
-               'Circuity', 'Percent 3-way Intersections',  'Particulate Matter (PM2.5) (ug/m^3)',
-               'Temperature (°F)', 'Precipitaton (inches)', 'Wind Speed (mph)', 'Density (people per sq. mi.)')
+     'Circuity', 'Percent 3-way Intersections',  'Particulate Matter (PM2.5) (ug/m^3)',
+     'Temperature (°F)', 'Precipitaton (inches)', 'Wind Speed (mph)', 'Ruggedness')
 
 names(sumdata) <- new_names
 datasummary_skim(sumdata, fmt = '%.3f')
@@ -172,10 +227,10 @@ datasummary_skim(sumdata, fmt = '%.3f')
 # Creating a correlation table to justify the HT instrument
 
 corvars <- c('edge_betweenness_centrality_mean', 'load_centrality_mean', 'circuity', 'pct_3way_intersections',
-             'PM2.5', 'TEMP', 'PRCP', 'WDSP', 'Density')
+   'PM2.5', 'TEMP', 'PRCP', 'WDSP', 'Rugged_Area')
 cordata <- regdata[,which(names(regdata) %in% corvars)]
 cordata <- cordata[which(complete.cases(cordata) == TRUE),]
-names(cordata) <- c('MEBC', 'MLC', 'Circuity', 'Pct 3-way', 'PM2.5', 'Temp', 'Precip', 'Wind', 'Density')
+names(cordata) <- c('MEBC', 'MLC', 'Circuity', 'Pct 3-way', 'PM2.5', 'Temp', 'Precip', 'Wind', 'Ruggedness')
 cor(cordata)
-cor(cordata[,which(names(cordata) %in% c('MEBC', 'MLC', 'Circuity', 'Pct 3-way', 'Density', 'PM2.5'))])
+cor(cordata[,which(names(cordata) %in% c('MEBC', 'MLC', 'Circuity', 'Pct 3-way', 'Ruggedness', 'PM2.5'))])
 
